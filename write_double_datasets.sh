cd05027366cf4635f68b48a51afe5fbfe7d8696c
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-01-07 14:31:38 -0500

    sh and py script for writing doubleImage beton files

---
+++ write_double_datasets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+write_dataset () {
+    write_path=$WRITE_DIR/doubleImage_${1}_${2}_${3}.ffcv
+    echo "Writing ImageNet doubleImage dataset to ${write_path}"
+    python write_double_atasets.py \
+        --cfg.dataset=imagenet \
+        --cfg.data_dir=$IMAGENET_DIR/train \
+        --cfg.write_path=$write_path \
+        --cfg.max_resolution=${1} \
+        --cfg.write_mode=proportion \
+        --cfg.compress_probability=${2} \
+        --cfg.jpeg_quality=$3
+}
+
+write_dataset $1 $2 $3
