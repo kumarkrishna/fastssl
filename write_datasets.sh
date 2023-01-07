73369d2c4b90fb78c20c252909c5d58985f5d58e
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-01-07 13:44:13 -0500

    added write_datasets.sh to create imagenet .beton

--- write_datasets.sh
+++ write_datasets.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-export IMAGENET_DIR=/network/datasets/imagenet.var/imagenet_torchvision/
-export export WRITE_DIR=/network/scratch/l/lindongy/ffcv_datasets/imagenet_256
-
-module load anaconda/3 python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
-source $HOME/testenv/bin/activate
 
 write_dataset () {
-    export IMAGENET_DIR=/network/datasets/imagenet.var/imagenet_torchvision/
-    export export WRITE_DIR=/network/scratch/l/lindongy/ffcv_datasets/imagenet_256
-
-    module load anaconda/3 python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
-    source $HOME/testenv/bin/activate
     write_path=$WRITE_DIR/${1}_${2}_${3}_${4}.ffcv
     echo "Writing ImageNet ${1} dataset to ${write_path}"
     python write_datasets.py \
