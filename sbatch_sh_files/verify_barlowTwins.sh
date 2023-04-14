43d55da1d3fbf784592e1ce429ce2e95cdf2b5ab
Arna Ghosh <arnatubaikgp@gmail.com>
2023-04-14 02:48:14 -0400

    Adding some comments in verify barlowTwins sh file to improve readability

--- sbatch_sh_files/verify_barlowTwins.sh
+++ sbatch_sh_files/verify_barlowTwins.sh
@@ -26,18 +26,16 @@ fi
 
 checkpt_dir=$SCRATCH/fastssl/checkpoints
 
+# Let's train a SSL (BarlowTwins) model with the above hyperparams
 python scripts/train_model.py --config-file configs/cc_barlow_twins.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.batch_size=$batch_size --training.track_alpha=True --training.log_interval=2
 
-# precache features, should take ~35 seconds
+# Let's precache features, should take ~35 seconds (rtx8000)
 python scripts/train_model.py --config-file configs/cc_precache.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.batch_size=$batch_size --training.model=resnet50feat
-# precache embeddings, should take ~35 seconds
+# Let's precache embeddings, should take ~35 seconds (rtx8000)
 python scripts/train_model.py --config-file configs/cc_precache.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.batch_size=$batch_size --training.model=resnet50proj
 
-# run linear eval on model
-# TODO: make linear eval compatible with precached features
-python scripts/train_model.py --config-file configs/cc_classifier.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.seed=1
-python scripts/train_model.py --config-file configs/cc_classifier.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.seed=2
-python scripts/train_model.py --config-file configs/cc_classifier.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.seed=3
+# run linear eval on precached features from model: using default seed 42
+python scripts/train_model.py --config-file configs/cc_classifier.yaml --training.lambd=$lambd --training.projector_dim=$pdim --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir --training.seed=42
 # dataset='stl10'
 
 cp $SLURM_TMPDIR/*.pth $checkpt_dir/resnet50_checkpoints/
