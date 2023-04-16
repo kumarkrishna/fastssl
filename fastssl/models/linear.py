40e93fe7edc85fb60e6b65adf3169be06f57d33b
Arna Ghosh <arnatubaikgp@gmail.com>
2023-04-16 17:13:40 -0400

    Changing forward function based on KK's suggestion in PR 28

--- fastssl/models/linear.py
+++ fastssl/models/linear.py
@@ -58,15 +58,22 @@ class LinearClassifier(nn.Module):
         
 
     def forward(self, x):
-        if 'proj' in self.bkey:
+        if self.backbone is not None:
             x = self.backbone(x)
+        if 'proj' in self.bkey:
             x = torch.flatten(x, start_dim=1)
-            feats = self.backbone.proj(x)
-        elif 'feat' in self.bkey:
-            feats = self.backbone(x)
-        elif len(self.bkey)==0:
-            # no backbone, i.e. Identity case
-            feats = self.backbone(x)
-        feats = torch.flatten(feats, start_dim=1)
+            x = self.backbone.proj(x)
+        feats = torch.flatten(x, start_dim=1)
+
+        # if 'proj' in self.bkey:
+        #     x = self.backbone(x)
+        #     x = torch.flatten(x, start_dim=1)
+        #     feats = self.backbone.proj(x)
+        # elif 'feat' in self.bkey:
+        #     feats = self.backbone(x)
+        # elif len(self.bkey)==0:
+        #     # no backbone, i.e. Identity case
+        #     feats = self.backbone(x)
+        # feats = torch.flatten(feats, start_dim=1)
         preds = self.fc(feats)
         return preds
