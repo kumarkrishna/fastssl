e6f9cbd534e746ce38fdb910f944c9843d92b47c
Arna Ghosh <arnatubaikgp@gmail.com>
2023-09-18 01:55:04 -0400

    Updating changes to sbatch file for hparam sweep for multipatch Barlow

--- sbatch_sh_files/multiPatch/multiPatch_barlowTwins_sweep.sh
+++ sbatch_sh_files/multiPatch/multiPatch_barlowTwins_sweep.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH --array=0-71%36
+#SBATCH --array=0-79%25
 #SBATCH --partition=long
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --mem=16GB
-#SBATCH --time=3:30:00
+#SBATCH --time=15:00:00
 #SBATCH --cpus-per-gpu=4
 #SBATCH --output=sbatch_out/multiPatch_barlow_sweep.%A.%a.out
 #SBATCH --error=sbatch_err/multiPatch_barlow_sweep.%A.%a.err
@@ -13,8 +13,9 @@
 module load anaconda/3
 conda activate ffcv_new
 
-lambd_arr=(0.001 0.004 0.008 0.01 0.02 0.05)
-pdim_arr=(1024 2048 3072)
+lambd_arr=(0.0001 0.0002 0.0004 0.0008 0.001 0.002 0.004 0.006 0.01 0.02)
+# pdim_arr=(1024 2048 3072)
+pdim_arr=(256 8192)
 augs_arr=(2 4 8 16)
 dataset='cifar10'
 if [ $dataset = 'stl10' ]
@@ -36,7 +37,8 @@ pdim=${pdim_arr[$pidx]}
 augs=${augs_arr[$aidx]}
 
 wandb_group='blake-richards'
-wandb_projname='multiPatch-Barlow-v3'
+# wandb_projname='multiPatch-Barlow-v3'
+wandb_projname='multiPatch-Barlow-sweep-ep100'
 
 model=resnet50proj
 
@@ -49,7 +51,7 @@ val_dpath=$SCRATCH/ffcv/ffcv_datasets/{dataset}/test.beton
 python scripts/train_model_multiPatch.py --config-file configs/cc_barlow_twins.yaml \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
                 --training.num_augmentations=$augs \
-                --training.epochs=10 --training.log_interval=10 \
+                --training.epochs=100 --training.log_interval=100 \
                 --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir \
                 --training.batch_size=$batch_size --training.model=$model \
                 --logging.use_wandb=True --logging.wandb_group=$wandb_group \
@@ -60,7 +62,7 @@ model=resnet50feat
 # Let's precache features, should take ~35 seconds (rtx8000)
 python scripts/train_model_multiPatch.py --config-file configs/cc_precache.yaml \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
-                --eval.num_augmentations_pretrain=$augs --eval.epoch=10 \
+                --eval.num_augmentations_pretrain=$augs --eval.epoch=100 \
                 --training.num_augmentations=16 \
                 --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir \
                 --training.batch_size=$batch_size --training.model=$model \
@@ -69,7 +71,7 @@ python scripts/train_model_multiPatch.py --config-file configs/cc_precache.yaml 
 # run linear eval on precached features from model: using default seed 42
 python scripts/train_model_multiPatch.py --config-file configs/cc_classifier.yaml \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
-                --eval.num_augmentations_pretrain=$augs --eval.epoch=10 \
+                --eval.num_augmentations_pretrain=$augs --eval.epoch=100 \
                 --training.num_augmentations=16 \
                 --training.dataset=$dataset --training.ckpt_dir=$checkpt_dir \
                 --training.seed=42 --training.model=$model \
