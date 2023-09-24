7ea7c7c3872d70d20816dbac8a285b19d327bc27
Arna Ghosh <arnatubaikgp@gmail.com>
2023-09-24 02:21:15 -0400

    Updating hparam sweep for correct implementation VICReg, setting mu=lamda now

--- sbatch_sh_files/resnet50_vicreg_sweep.sh
+++ sbatch_sh_files/resnet50_vicreg_sweep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --array=0-80%50
+#SBATCH --array=0-17%20
 #SBATCH --partition=long
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --mem=16GB
@@ -20,10 +20,11 @@ WANDB__SERVICE_WAIT=300
 # pdim_arr=(2048 4096)
 # lambd_arr=(150.0 200.0 250.0 500.0 750.0 1000.0)
 # mu_arr=(150.0 200.0 250.0 500.0 750.0 1000.0)
-# pdim_arr=(8192)
-lambd_arr=(1.0 5.0 10.0 25.0 50.0 75.0 100.0 500.0 1000.0)
-mu_arr=(1.0 5.0 10.0 25.0 50.0 75.0 100.0 500.0 1000.0)
-pdim_arr=(512)
+lambd_arr=(200.0 400.0 600.0 800.0 1200.0 1400.0 1600.0 1800.0 2000.0)
+pdim_arr=(4096 8192)
+# lambd_arr=(1.0 5.0 10.0 25.0 50.0 75.0 100.0 500.0 1000.0)
+# mu_arr=(1.0 5.0 10.0 25.0 50.0 75.0 100.0 500.0 1000.0)
+# pdim_arr=(512)
 augs_arr=(2)
 dataset='cifar10'
 if [ $dataset = 'stl10' ]
@@ -33,25 +34,39 @@ else
     batch_size=512
 fi
 
+# lenL=${#lambd_arr[@]}
+# lenM=${#mu_arr[@]}
+# lenP=${#pdim_arr[@]}
+# lenA=${#augs_arr[@]}
+# lenLMP=$((lenL*lenM*lenP))
+# lenLM=$((lenL*lenM))
+# aidx=$((SLURM_ARRAY_TASK_ID/lenLMP))
+# pmlidx=$((SLURM_ARRAY_TASK_ID%lenLMP))
+# pidx=$((pmlidx/lenLM))
+# mlidx=$((pmlidx%lenLM))
+# midx=$((mlidx/lenL))
+# lidx=$((mlidx%lenL))
+# lambd=${lambd_arr[$lidx]}
+# mu=${mu_arr[$midx]}
+# pdim=${pdim_arr[$pidx]}
+# augs=${augs_arr[$aidx]}
+
+# setting mu = lambda for sweeps
 lenL=${#lambd_arr[@]}
-lenM=${#mu_arr[@]}
 lenP=${#pdim_arr[@]}
 lenA=${#augs_arr[@]}
-lenLMP=$((lenL*lenM*lenP))
-lenLM=$((lenL*lenM))
-aidx=$((SLURM_ARRAY_TASK_ID/lenLMP))
-pmlidx=$((SLURM_ARRAY_TASK_ID%lenLMP))
-pidx=$((pmlidx/lenLM))
-mlidx=$((pmlidx%lenLM))
-midx=$((mlidx/lenL))
-lidx=$((mlidx%lenL))
+lenLP=$((lenL*lenP))
+aidx=$((SLURM_ARRAY_TASK_ID/lenLP))
+plidx=$((SLURM_ARRAY_TASK_ID%lenLP))
+pidx=$((pmlidx/lenL))
+lidx=$((pmlidx%lenL))
 lambd=${lambd_arr[$lidx]}
-mu=${mu_arr[$midx]}
+mu=${mu_arr[$lidx]}
 pdim=${pdim_arr[$pidx]}
 augs=${augs_arr[$aidx]}
 
 wandb_group='blake-richards'
-wandb_projname='VICReg-resnet50-hparam'
+wandb_projname='VICReg-resnet50-hparam-v2'
 
 checkpt_dir=$SCRATCH/fastssl/checkpoints_vicreg
 train_dpath=$SCRATCH/ffcv/ffcv_datasets/{dataset}/train.beton
