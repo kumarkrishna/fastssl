ddab2e34c88421b3843872cbab32a4e5d6036777
Arna Ghosh <arnatubaikgp@gmail.com>
2024-05-18 15:09:55 -0400

    File rename

--- sbatch_sh_files/multiPatch/multiPatch_resnet18_barlow_imagenet_distributed.sh
+++ sbatch_sh_files/multiPatch/multiPatch_resnet18_barlow_imagenet_distributed.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 #####SBATCH --array=0-7%20
 #####SBATCH --array=0-13%10
-#SBATCH --array=0-10%11
+####SBATCH --array=0-1%11
+#SBATCH --array=4
 #SBATCH --partition=long
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:2
 #SBATCH --mem=32GB
 ######SBATCH --time=18:00:00
-#SBATCH --time=8:00:00
-#SBATCH --cpus-per-gpu=6
-#SBATCH --output=sbatch_out/resnet18_barlowtwins_imagenet_sweep.%A.%a.out
-#SBATCH --error=sbatch_err/resnet18_barlowtwins_imagenet_sweep.%A.%a.err
-#SBATCH --job-name=resnet18_barlowtwins_imagenet_sweep
+#SBATCH --time=16:00:00
+#SBATCH --cpus-per-gpu=4
+#SBATCH --output=sbatch_out/resnet18_mp_barlowtwins_imagenet_sweep.%A.%a.out
+#SBATCH --error=sbatch_err/resnet18_mp_barlowtwins_imagenet_sweep.%A.%a.err
+#SBATCH --job-name=resnet18_mp_barlowtwins_imagenet_sweep
 
 . /etc/profile
 module load anaconda/3
@@ -19,38 +20,42 @@ conda activate ffcv_new
 WANDB__SERVICE_WAIT=300
 which python
 
-lambd_arr=(0.007 0.007 0.01 0.01 0.02 0.03 0.04 0.01 0.02 0.04 0.08)
-pdim_arr=(2048 1024 1024 512 512 512 512 256 256 256 256)
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR="127.0.0.1"
+
+lambd_arr=(0.02 0.01 0.02 0.04 0.01 0.02 0.04)
+augs_arr=(2 4 4 4 8 8 8)
 lenL=${#lambd_arr[@]}
-lenP=${#pdim_arr[@]}
-pidx=$SLURM_ARRAY_TASK_ID
+lenA=${#augs_arr[@]}
+aidx=$SLURM_ARRAY_TASK_ID
 lidx=$SLURM_ARRAY_TASK_ID
 
 lambd=${lambd_arr[$lidx]}
-pdim=${pdim_arr[$pidx]}
-lambd=0.005
-pdim=2048
+num_augs=${augs_arr[$aidx]}
+# lambd=0.02
+pdim=512
 # dataset='imagenet'
 dataset='imagenet100'
-num_augs=4
-batch_size=512
+batch_size=128
 
-wandb_group='blake-richards'
+wandb_group='eigengroup'
 # wandb_projname='Barlow-resnet18-hparam-imagenet'
-wandb_projname='Barlow-resnet18-hparam-imagenet100'
+wandb_projname='multiPatch_Barlow-resnet18-hparam-imagenet100'
 
 # checkpt_dir=$SCRATCH/fastssl/checkpoints_imagenet96_barlow
-checkpt_dir=$SCRATCH/fastssl/checkpoints_imagenet100_barlow
+checkpt_dir=$SCRATCH/fastssl/checkpoints_imagenet100_barlow_mp
 train_dpath=$SCRATCH/ffcv/ffcv_datasets/$dataset/train_no10k_500_0.50_90.ffcv
 val_dpath=$SCRATCH/ffcv/ffcv_datasets/$dataset/val_500_0.50_90.ffcv
-num_workers=6
+num_workers=4
 
 model=resnet18proj
 # epochs=30
-epochs=30
+epochs=100
 log_epochs=5
+
 # Let's train a SSL (BarlowTwins) model with the above hyperparams
-$HOME/.conda/envs/ffcv_new/bin/python scripts/train_model_imagenet_distributed.py \
+$HOME/.conda/envs/ffcv_new/bin/python -m torch.distributed.run --nproc_per_node 2 \
+                scripts/train_model_imagenet_distributed.py \
                 --config-file configs/cc_barlow_twins.yaml \
                 --training.model=$model --training.dataset=$dataset \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
@@ -60,7 +65,8 @@ $HOME/.conda/envs/ffcv_new/bin/python scripts/train_model_imagenet_distributed.p
                 --logging.use_wandb=True --logging.wandb_group=$wandb_group \
                 --logging.wandb_project=$wandb_projname \
                 --training.train_dataset=$train_dpath --training.val_dataset=$val_dpath \
-                --training.num_workers=$num_workers
+                --training.num_workers=$num_workers \
+                --eval.num_augmentations_pretrain=$num_augs --training.distributed=True
 
 model=resnet18feat
 
@@ -71,10 +77,10 @@ $HOME/.conda/envs/ffcv_new/bin/python scripts/train_model.py \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
                 --training.batch_size=$batch_size --training.seed=42 \
                 --training.ckpt_dir=$checkpt_dir --eval.num_augmentations_pretrain=$num_augs \
-                --eval.epoch=$epochs \
+                --eval.epoch=$epochs --training.num_workers=$num_workers \
                 --training.train_dataset=$train_dpath --training.val_dataset=$val_dpath
 
-# run linear eval on precached features from model: using default seed 42
+# # run linear eval on precached features from model: using default seed 42
 $HOME/.conda/envs/ffcv_new/bin/python scripts/train_model.py \
                 --config-file configs/cc_classifier.yaml \
                 --training.model=$model --training.dataset=$dataset \
