0ae965ee389d60edc40e8ae4949f27d0a2c03325
Arna Ghosh <arnatubaikgp@gmail.com>
2024-08-26 05:26:59 -0400

    Updating sbatch script for multipatch fractrain exps for imagenet-100

--- sbatch_sh_files/multiPatch/multiPatch_resnet18_barlow_imagenet_fracTrain_distributed.sh
+++ sbatch_sh_files/multiPatch/multiPatch_resnet18_barlow_imagenet_fracTrain_distributed.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH --array=11%10
+#SBATCH --array=0-11%10
 #SBATCH --partition=long
-#SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100l:2
 #SBATCH --mem=32GB
-#SBATCH --time=8:00:00
+#SBATCH --time=10:00:00
 #SBATCH --cpus-per-gpu=4
 #SBATCH --output=sbatch_out/multiPatch_barlow_fracTrain_imagenet100_results.%A.%a.out
 #SBATCH --error=sbatch_err/multiPatch_barlow_fracTrain_imagenet100_results.%A.%a.err
@@ -20,25 +20,36 @@ export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 export MASTER_ADDR="127.0.0.1"
 echo $SLURM_JOBID, $SLURM_ARRAY_TASK_ID, $MASTER_PORT, $MASTER_ADDR
 
-lambd_arr=(0.02 0.002 0.004 0.007 0.01 0.02 0.04 0.002 0.004 0.006 0.008 0.01 0.02 0.012 0.014 0.015 0.016 0.018)
-augs_arr=(2 4 4 4 4 4 4 8 8 8 8 8 8 8 8 8 8 8)
+# lambd_arr=(0.02 0.002 0.004 0.007 0.01 0.02 0.04 0.002 0.004 0.006 0.008 0.01 0.02 0.012 0.014 0.015 0.016 0.018)
+# augs_arr=(2 4 4 4 4 4 4 8 8 8 8 8 8 8 8 8 8 8)
+# lenL=${#lambd_arr[@]}
+# lenA=${#augs_arr[@]}
+# aidx=$SLURM_ARRAY_TASK_ID
+# lidx=$SLURM_ARRAY_TASK_ID
+
+pdim_arr=(8192   512  512   512)
+lambd_arr=(0.001 0.02 0.004 0.01)
+augs_arr=(2      2    4     8)
 lenL=${#lambd_arr[@]}
 lenA=${#augs_arr[@]}
-aidx=$SLURM_ARRAY_TASK_ID
-lidx=$SLURM_ARRAY_TASK_ID
+sidx=$((SLURM_ARRAY_TASK_ID/lenL))
+cfgidx=$((SLURM_ARRAY_TASK_ID%lenL))
+pidx=$cfgidx
+lidx=$cfgidx
+aidx=$cfgidx
 
 lambd=${lambd_arr[$lidx]}
 num_augs=${augs_arr[$aidx]}
-# lambd=0.02
-pdim=512
+pdim=${pdim_arr[$pidx]}
 # dataset='imagenet'
 dataset='imagenet100'
-if [ $num_augs = 8 ]
-then
-    batch_size=125
-else
-    batch_size=128
-fi
+# if [ $num_augs = 8 ]
+# then
+#     batch_size=125
+# else
+#     batch_size=128
+# fi
+batch_size=128
 
 wandb_group='eigengroup'
 wandb_projname='multiPatch_Barlow_fractrain-resnet18-hparam-imagenet100'
@@ -70,7 +81,7 @@ $HOME/.conda/envs/ffcv_new/bin/python -m torch.distributed.run \
                 --config-file configs/cc_barlow_twins.yaml \
                 --training.model=$model --training.dataset=$dataset \
                 --training.lambd=$lambd --training.projector_dim=$pdim \
-                --training.batch_size=$batch_size --training.seed=42 \
+                --training.batch_size=$batch_size --training.seed=$sidx \
                 --training.ckpt_dir=$checkpt_dir --training.num_augmentations=$num_augs \
                 --training.epochs=$epochs --training.log_interval=$log_epochs \
                 --logging.use_wandb=True --logging.wandb_group=$wandb_group \
@@ -92,7 +103,7 @@ do
                     --config-file configs/cc_precache.yaml \
                     --training.model=$model --training.dataset=$dataset \
                     --training.lambd=$lambd --training.projector_dim=$pdim \
-                    --training.batch_size=$batch_size --training.seed=42 \
+                    --training.batch_size=$batch_size --training.seed=$sidx \
                     --training.ckpt_dir=$checkpt_dir --eval.num_augmentations_pretrain=$num_augs \
                     --eval.epoch=$ep --training.num_workers=$num_workers \
                     --training.train_dataset=$train_dpath --training.val_dataset=$val_dpath
@@ -102,7 +113,7 @@ do
                     --config-file configs/cc_classifier.yaml \
                     --training.model=$model --training.dataset=$dataset \
                     --training.lambd=$lambd --training.projector_dim=$pdim \
-                    --training.batch_size=$batch_size --training.seed=42 \
+                    --training.batch_size=$batch_size --training.seed=$sidx \
                     --training.ckpt_dir=$checkpt_dir --eval.num_augmentations_pretrain=$num_augs \
                     --eval.epoch=$ep \
                     --logging.use_wandb=True --logging.wandb_group=$wandb_group \
